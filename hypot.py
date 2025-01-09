12a76eb4a92cfbb17f41ec59483521a02207a28e
golololologol <50058139+golololologol@users.noreply.github.com>
2025-01-09 20:30:46 +0300

    Ugraded hypot.py

--- hypot.py
+++ hypot.py
@@ -1,4 +1,11 @@
 import math
 
-def hyp(x, y):
+def hyp(x: float, y: float) -> float:
+    """
+    Calculate the hypotenuse of a right triangle using math.hypot(x, y).
+    
+    :param x: Length of one side
+    :param y: Length of the other side
+    :return: Hypotenuse as a float
+    """
     return math.hypot(x, y)
