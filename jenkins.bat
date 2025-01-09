9f47c54ffc1b64415e7dc5640879bec763e049a8
golololologol <50058139+golololologol@users.noreply.github.com>
2025-01-09 20:38:41 +0300

    Update jenkins.bat to also test the code with Pylint and flake8

--- jenkins.bat
+++ jenkins.bat
@@ -1,12 +1,18 @@
 @echo off
 
-echo Installing coverage...
-pip install coverage
+echo Installing coverage, pylint, and flake8...
+pip install coverage pylint flake8
 
 echo Running coverage...
 coverage run -m unittest test_hypot.py
 echo Coverage report:
 coverage report -m
 coverage erase
+
+echo Running pylint...
+pylint hypot.py test_hypot.py
+
+echo Running flake8...
+flake8 hypot.py test_hypot.py
 
 exit /b 0
