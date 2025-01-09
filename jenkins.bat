40cbd7bf4ec8fb3e30c19ad3c510ed0bf2a700bb
golololologol <50058139+golololologol@users.noreply.github.com>
2025-01-09 21:03:15 +0300

    Update jenkins.bat to pull requirements from requirements.txt

--- jenkins.bat
+++ jenkins.bat
@@ -1,14 +1,17 @@
 @echo off
 
-echo Installing coverage, pylint, and flake8...
-pip install coverage pylint flake8
+REM Install all dependencies from requirements.txt
+echo Installing dependencies...
+pip install -r requirements.txt
 
+REM Run coverage
 echo Running coverage...
 coverage run -m unittest test_hypot.py
 echo Coverage report:
 coverage report -m
 coverage erase
 
+REM Run pylint and flake8
 echo Running pylint...
 pylint hypot.py test_hypot.py
 
